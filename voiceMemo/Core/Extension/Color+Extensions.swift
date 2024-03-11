9d5a0a4b9134b7e8841c8b890133ebbd380b3511
이정훈 <jhmh0226@idynamics.co.kr>
2024-03-12 00:12:22 +0900

    20240312

---
+++ voiceMemo/Core/Extension/Color+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Color+extensions.swift
+//  voiceMemo
+//
+//  Created by 이정훈 on 3/11/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backGroundGreen = Color("customBackgroundGreen")
+    static let black = Color("customBlack")
+    static let coolGray = Color("customCoolGray")
+    static let gray0 = Color("customGray0")
+    static let gray1 = Color("customGray1")
+    static let gray2 = Color("customGray2")
+    static let green = Color("customGreen")
+    static let iconGray = Color("customIconGray")
+    static let orange = Color("customOrange")
+    static let sky = Color("customSky")
+    static let white = Color("customWhite")
+}
---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by 이정훈 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
